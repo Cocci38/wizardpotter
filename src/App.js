cd82d7d1ef0e5165fcfc17a8640c49dc44010de6
Cocci38 <sandra.villot038@gmail.com>
2023-02-20 21:42:30 +0100

    first comit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import AllPersoList from './pages/AllPersoList';
+import Contact from './pages/Contact';
+import Home  from "./pages/Home";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    /**
+     * Création du routeur
+     * path = l'url de notre page
+     * element = le composant ou page que l'on veut chercher
+     */
+    <div>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/allperso' element={<AllPersoList/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
